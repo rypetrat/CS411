8afc2b0baaf4e037d645d444fdad5a665a355786
rypetrat <rypetrat@bu.edu>
2024-03-04 18:25:19 -0500

    addded external openAI api call from backend and will display it on frontend

--- healthdiagnosticback/server.js
+++ healthdiagnosticback/server.js
@@ -13,27 +13,48 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Health Diagonstic Tool backend server!');
 });
 
-// local route
-app.get('/api/data', (req, res) => {
+// test route
+app.get('/api/test', (req, res) => {
   res.json({ text: 'hello frontend!' });
 });
 
-// external cannlytics route
-// app.get('/api/question', async (req, res) => {
-//   try {
-//     const response = await axios.get('put api address here', {
-//       // 5 seconds should be fine right
-//       timeout: 5000, 
-//     });
-//     res.json(response.data);
-//   } catch (error) {
-//     if (error.code === 'ECONNABORTED') {
-//       res.status(500).json({ error: 'Request timed out' });
-//     } else {
-//       res.status(500).json({ error: 'Failed to fetch data' });
-    
-//   }}
-// });
+// openAI chatGPT4 API route
+app.get('/api/data', async (req, res) => {
+  const options = {
+    method: 'POST',
+    url: 'https://chatgpt-42.p.rapidapi.com/conversationgpt4',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '624673b5b3mshd8d8edb53803e49p1a2ac4jsnd52e961782d0',
+      'X-RapidAPI-Host': 'chatgpt-42.p.rapidapi.com'
+    },
+    data: {
+      messages: [
+        {
+          role: 'user',
+          content: 'what are the colors of the american flag?'
+        }
+      ],
+      system_prompt: '',
+      temperature: 0.9,
+      top_k: 5,
+      top_p: 0.9,
+      max_tokens: 256,
+      web_access: false
+    }};
+  
+  try {
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    if (error.code === 'ECONNABORTED') {
+      res.status(500).json({ error: 'Request timed out' });
+    } 
+      else {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
--- healthdiagnosticfront/src/App.js
+++ healthdiagnosticfront/src/App.js
@@ -12,7 +12,7 @@ function App() {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setMessage(data.text);
+        setMessage(data.result);
       } catch (error) {
         console.error('Error:', error);
       }

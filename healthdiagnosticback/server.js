32ec61f08221cdffca7d7490f5d532f39ed53c9f
rypetrat <rypetrat@bu.edu>
2024-03-04 13:05:29 -0500

    created initial backend server, communicates with frontend

---
+++ healthdiagnosticback/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+// Enable CORS for all requests
+app.use(cors());
+
+const PORT = 5000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Health Diagonstic Tool backend server!');
+});
+
+// local route
+app.get('/api/data', (req, res) => {
+  res.json({ text: 'hello frontend!' });
+});
+
+// external cannlytics route
+// app.get('/api/question', async (req, res) => {
+//   try {
+//     const response = await axios.get('put api address here', {
+//       // 5 seconds should be fine right
+//       timeout: 5000, 
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     if (error.code === 'ECONNABORTED') {
+//       res.status(500).json({ error: 'Request timed out' });
+//     } else {
+//       res.status(500).json({ error: 'Failed to fetch data' });
+    
+//   }}
+// });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- healthdiagnosticfront/src/App.js
+++ healthdiagnosticfront/src/App.js
@@ -1,14 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/data');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setMessage(data.text);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-text">
-
-        <>
-          Hey team welcome to the frontend of our Health Diagnostic tool!
-        </>
-
+        <h1>Hey team welcome to the frontend of our Health Diagnostic tool!</h1>
+        <p>Message from the backend server: {message}</p>
       </header>
     </div>
   );

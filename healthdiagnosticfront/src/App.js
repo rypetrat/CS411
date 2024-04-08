8e359419900e3621071fda62ed8cf77d8350b186
rypetrat <rypetrat@bu.edu>
2024-04-08 14:14:31 -0400

    made a welcome page with a buton that directs to the actual webapp which i made have a dynamic search bar for user inputs that then get queried by the backend through the api call

--- healthdiagnosticfront/src/App.js
+++ healthdiagnosticfront/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import './TextInput';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [userInput, setUserInput] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,11 +23,48 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUserInput(''); // Clear the search bar
+    try {
+      const response = await fetch('http://localhost:5000/api/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: userInput }),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setMessage(data.result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-text">
-        <h1>Hey team welcome to the frontend of our Health Diagnostic tool!</h1>
-        <p>Message from the backend server: {message}</p>
+        <h1>Health Diagnostic tool</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form__group field">
+            <input
+              type="input"
+              className="form__field"
+              placeholder="Name"
+              name="name"
+              id="name"
+              required value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+            <label htmlFor="name" className="form__label">
+              Symptoms
+            </label>
+          </div>
+        </form>
+        {message && <p>Response: {message}</p>}
       </header>
     </div>
   );
---
+++ healthdiagnosticfront/src/GetStartedPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
+const GetStartedPage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/search');
+  };
+
+  return (
+    <div className="App">
+      <header className="App-text">
+        <h1>Welcome to the Health Diagnostic Tool!</h1>
+        <p>This is a web app that anyone can help diagnose any illness based on symptoms and basic health information</p>
+        <button class="button1" onClick={handleClick} ><span>Get Started </span></button>
+      </header>
+    </div>
+  );
+};
+
+export default GetStartedPage;
